47eef6b73579dd355ebc3afd977a212a08def6a6
hyeonsangjeon <wingnut0310@gmail.com>
2017-12-01 05:37:25 +0900

    add docker example

---
+++ docker-example/src/main/java/com/docker/controller/DockerController.java
@@ -0,0 +1,23 @@
+package com.docker.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class DockerController {
+  private static final Logger logger = LoggerFactory.getLogger(DockerController.class);
+  
+  //Index
+  @RequestMapping(value="/", method = RequestMethod.GET)
+  public ResponseEntity<String> index(){
+	  logger.debug("HttpRequestServlet Route");
+	  return new ResponseEntity<>("Welcome Springboot docker running", HttpStatus.OK);
+  }
+       
+}
