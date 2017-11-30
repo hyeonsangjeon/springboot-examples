cd84751a6578b21ea38baf68cc1c19debb98d505
hyeonsangjeon <wingnut0310@gmail.com>
2017-11-30 10:03:12 +0900

    add flyuway example

---
+++ actuator-example/src/main/java/com/actuator/controller/ActuatorController.java
@@ -0,0 +1,39 @@
+package com.actuator.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import com.actuator.repository.BookRepository;
+
+
+@RestController
+public class ActuatorController {
+  private static final Logger logger = LoggerFactory.getLogger(ActuatorController.class);
+  
+  //Index
+  @RequestMapping(value="/", method = RequestMethod.GET)
+  public ResponseEntity<String> index(){	  	  
+	  return new ResponseEntity<>("Welcome Springboot flyway", HttpStatus.OK);
+  }
+  
+  //@Bean CommandLineRunner means that it will run after the springboot autoconfiguration.
+  @Bean
+  CommandLineRunner findAll(BookRepository bookRepo) {	  
+	  return args -> {		  		 
+		  bookRepo.findAll().forEach(book ->		  
+		      logger.debug("[MIGRATION DB INFO] : "+book.toString())
+		  );
+	  };
+	  
+  }
+  
+  
+  
+
+}
---
+++ actuator-example/src/main/resources/db/migration/V2__init.sql
@@ -0,0 +1,7 @@
+
+insert into book(id, name, author, pages) values(null, 'Disparity based space variant image deblurring, Signal Processing: Image Communication', 'ChangsooJe,HyeonSangJeon,ChangHwanSon,HyungMinPark', 792);
+insert into book(id, name, author, pages) values(null, 'A Speech Enhancement Algorithm Based on Blind Signal Cancelation in Diffuse Noise Environments: IEICE TRANS', 'Jaesik Hwang, Jaepil Seo, Ji-Won Cho and Hyung-Min Park',407);
+insert into book(id, name, author, pages) values(null, 'Preprocessing of independent vector analysis using feed-forward network for robust speech recognition : Lecture Notes in Computer Science', 'Myungwoo Oh and Hyung-Min Park', 366);
+insert into book(id, name, author, pages) values(null, 'An efficient method to remove boundary defects in block-based deblurring algorithm using multiple images: Image Processing and Image Understanding', 'Hyeon-Sang Jeon, Chang-Hwan Son, and Hyung-Min Park', 0);
+insert into book(id, name, author, pages) values(null, 'Music noise reduction based on normalized gradient coefficients and non-negative matrix factorization : Conference of Korean Society of Speech Sciences', 'Il-Young Jeong and Hyung-Min Park', 0);
+    
