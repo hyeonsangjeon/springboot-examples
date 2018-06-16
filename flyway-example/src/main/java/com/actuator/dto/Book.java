2953a6ed43189f69f1ed7e0e5e9947abb547f129
hyeonsangjeon <wingnut0310@gmail.com>
2018-06-17 00:35:59 +0900

    indent

--- flyway-example/src/main/java/com/actuator/dto/Book.java
+++ flyway-example/src/main/java/com/actuator/dto/Book.java
@@ -3,6 +3,7 @@ package com.actuator.dto;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -13,13 +14,13 @@ import lombok.ToString;
 @ToString
 @Entity
 public class Book {
-	private static final long serialVersionUID = 8203836758273948712L;  
-	
-	@Id
-	@GeneratedValue
-	private Long id ;	
-	private String name = "";
-	private String author = "";
+    private static final long serialVersionUID = 8203836758273948712L;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name = "";
+    private String author = "";
     private int pages;
-        
+
 }
