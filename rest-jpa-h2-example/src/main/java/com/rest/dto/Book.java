9963a0721e75380fe1a31eff69c686aa42752230
hyeonsangjeon <wingnut0310@gmail.com>
2018-06-17 00:58:48 +0900

    indent

--- rest-jpa-h2-example/src/main/java/com/rest/dto/Book.java
+++ rest-jpa-h2-example/src/main/java/com/rest/dto/Book.java
@@ -2,11 +2,7 @@ package com.rest.dto;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-
-
 import javax.persistence.Id;
-
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
